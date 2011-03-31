0840e4d62691b6c74e80a66989633469b70863af
Florian Schulz <hanbei@hanbei.de>
2011-03-31 21:32:46 +0200

    cleaned up test

--- src/main/java/de/hanbei/httpserver/response/ResponseBuilder.java
+++ src/main/java/de/hanbei/httpserver/response/ResponseBuilder.java
@@ -1,8 +1,5 @@
 package de.hanbei.httpserver.response;
 
-import de.hanbei.httpserver.common.Constants;
-import de.hanbei.httpserver.common.Status;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -11,100 +8,98 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-import org.apache.log4j.lf5.util.StreamUtils;
+import de.hanbei.httpserver.common.Constants;
+import de.hanbei.httpserver.common.Status;
 
 /**
- * Created by IntelliJ IDEA.
- * User: hanbei
- * Date: 26.02.11
- * Time: 13:01
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: hanbei Date: 26.02.11 Time: 13:01 To change
+ * this template use File | Settings | File Templates.
  */
 public class ResponseBuilder {
 
-    private Response response;
+	private Response response;
 
-    private SimpleDateFormat dateFormat;
+	private SimpleDateFormat dateFormat;
 
+	ResponseBuilder(Response response) {
+		this.response = response;
+		dateFormat = new SimpleDateFormat("EEE, dd MMM yyyyy HH:mm:ss z");
+		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+	}
 
-    ResponseBuilder(Response response) {
-        this.response = response;
-        dateFormat = new SimpleDateFormat("EEE, dd MMM yyyyy HH:mm:ss z");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-    }
+	public Response build() {
+		return response;
+	}
 
-    public Response build() {
-        return response;
-    }
+	public ResponseBuilder status(int status) {
+		response.setStatus(new Status(status));
+		return this;
+	}
 
-    public ResponseBuilder status(int status) {
-        response.setStatus(new Status(status));
-        return this;
-    }
+	public ResponseBuilder status(Status status) {
+		response.setStatus(status);
+		return this;
+	}
 
-    public ResponseBuilder status(Status status) {
-        response.setStatus(status);
-        return this;
-    }
+	public ResponseBuilder version(String version) {
+		response.setHttpVersion(version);
+		return this;
+	}
 
-    public ResponseBuilder version(String version) {
-        response.setHttpVersion(version);
-        return this;
-    }
+	public ResponseBuilder content(Object content) {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		try {
+			ObjectOutputStream objectOut = new ObjectOutputStream(bytes);
+			objectOut.writeObject(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		content(bytes.toByteArray());
+		return this;
+	}
 
-    public ResponseBuilder content(Object content) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        try {
-            ObjectOutputStream objectOut = new ObjectOutputStream(bytes);
-            objectOut.writeObject(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        content(bytes.toByteArray());
-        return this;
-    }
+	public ResponseBuilder content(String content) {
+		content(content.getBytes());
+		return this;
+	}
 
-    public ResponseBuilder content(String content) {
-        content(content.getBytes());
-        return this;
-    }
+	public ResponseBuilder content(byte[] content) {
+		response.getContent().setContent(content);
+		response.getContent().setLength(content.length);
+		return this;
+	}
 
-    public ResponseBuilder content(byte[] content) {
-        response.getContent().setContent(content);
-        response.getContent().setLength(content.length);
-        return this;
-    }
+	public ResponseBuilder expires(Date expires) {
+		response.getHeader().addParameter(Constants.EXPIRES,
+				dateFormat.format(expires));
+		return this;
+	}
 
-    public ResponseBuilder expires(Date expires) {
-        response.getHeader().addParameter(Constants.EXPIRES, dateFormat.format(expires));
-        return this;
-    }
+	public ResponseBuilder header(String name, String value) {
+		response.getHeader().addParameter(name, value);
+		return this;
+	}
 
-    public ResponseBuilder header(String name, String value) {
-        response.getHeader().addParameter(name, value);
-        return this;
-    }
+	public ResponseBuilder language(String language) {
+		response.getHeader().addAcceptLanguage(language);
+		return this;
+	}
 
-    public ResponseBuilder language(String language) {
-        response.getHeader().addAcceptLanguage(language);
-        return this;
-    }
+	public ResponseBuilder lastModified(Date lastModified) {
+		response.getHeader().addParameter(Constants.LAST_MODIFIED,
+				dateFormat.format(lastModified));
+		return this;
+	}
 
-    public ResponseBuilder lastModified(Date lastModified) {
-        response.getHeader().addParameter(Constants.LAST_MODIFIED, dateFormat.format(lastModified));
-        return this;
-    }
+	public ResponseBuilder location(URI location) {
+		response.getContent().setLocation(location);
+		return this;
+	}
 
-    public ResponseBuilder location(URI location) {
-        response.getContent().setLocation(location);
-        return this;
-    }
-
-    public ResponseBuilder type(String type) {
-        response.getHeader().addAcceptMimetype(type);
-        response.getContent().setMimetype(type);
-        return this;
-    }
+	public ResponseBuilder type(String type) {
+		response.getHeader().addAcceptMimetype(type);
+		response.getContent().setMimetype(type);
+		return this;
+	}
 
 }
-
--- src/test/java/de/hanbei/httpserver/response/ResponseBuilderTest.java
+++ src/test/java/de/hanbei/httpserver/response/ResponseBuilderTest.java
@@ -1,57 +1,52 @@
 package de.hanbei.httpserver.response;
 
-import de.hanbei.httpserver.common.Status;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.hamcrest.collection.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import de.hanbei.httpserver.common.Status;
+
 /**
- * Created by IntelliJ IDEA.
- * User: hanbei
- * Date: 27.02.11
- * Time: 12:58
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: hanbei Date: 27.02.11 Time: 12:58 To change
+ * this template use File | Settings | File Templates.
  */
 public class ResponseBuilderTest {
 
-    private ResponseBuilder responseBuilder;
+	@Before
+	public void setUp() throws Exception {
+	}
 
-    @Before
-    public void setUp() throws Exception {
-        responseBuilder = new ResponseBuilder(new Response());
-    }
+	@Test
+	public void testOk() throws Exception {
+		Response response = Response.ok().build();
+		assertEquals(Status.OK, response.getStatus());
+	}
 
-    @Test
-    public void testOk() throws Exception {
-        Response response = Response.ok().build();
-        assertEquals(Status.OK, response.getStatus());
-    }
+	@Test
+	public void testStatus() throws Exception {
+		Response response = Response.status(Status.NOT_FOUND).build();
+		assertEquals(Status.NOT_FOUND, response.getStatus());
+	}
 
-    @Test
-    public void testStatus() throws Exception {
-        Response response = Response.status(Status.NOT_FOUND).build();
-        assertEquals(Status.NOT_FOUND, response.getStatus());
-    }
+	@Test
+	public void testLanguage() throws Exception {
+		Response response = Response.ok().language("en").build();
+		assertThat(response.getHeader().getAcceptLanguages(), hasItems("en"));
+		assertEquals("HTTP/1.1 200 OK\nAccept-Language: en\n\n", response
+				.toString());
+	}
 
-    @Test
-    public void testLanguage() throws Exception {
-        Response response = Response.ok().language("en").build();
-        assertThat(response.getHeader().getAcceptLanguages(), hasItems("en"));
-        assertEquals("HTTP/1.1 200 OK\nAccept-Language: en\n\n", response.toString());
-    }
-
-    @Test
-    public void contentIsAddedRight() throws Exception {
-        Response response = Response.ok().content("TestContent").build();
-        assertEquals(11, response.getContent().getLength());
-        assertEquals("TestContent", new String(response.getContent().getContent()));
-        assertEquals("HTTP/1.1 200 OK\n" +
-                "Content-Length: 11\n" +
-                "\n" +
-                "TestContent\n", response.toString());
-    }
+	@Test
+	public void contentIsAddedRight() throws Exception {
+		Response response = Response.ok().content("TestContent").build();
+		assertEquals(11, response.getContent().getLength());
+		assertEquals("TestContent", new String(response.getContent()
+				.getContent()));
+		assertEquals("HTTP/1.1 200 OK\n" + "Content-Length: 11\n" + "\n"
+				+ "TestContent\n", response.toString());
+	}
 
 }
