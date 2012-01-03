9d8f50430ee269a6a710e1ff37d8cd6ccf882abe
hanbei <hanbei@dione.(none)>
2012-01-03 20:29:27 +0100

    copyright notice added to files

--- src/main/java/de/hanbei/httpserver/exceptions/ContentException.java
+++ src/main/java/de/hanbei/httpserver/exceptions/ContentException.java
@@ -1,12 +1,18 @@
+/* Copyright 2011 Florian Schulz
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 package de.hanbei.httpserver.exceptions;
 
-/**
- * Created by IntelliJ IDEA.
- * User: fschulz
- * Date: 03.06.11
- * Time: 11:51
- * To change this template use File | Settings | File Templates.
- */
 public class ContentException extends RuntimeException {
 
     public ContentException(Throwable throwable) {
